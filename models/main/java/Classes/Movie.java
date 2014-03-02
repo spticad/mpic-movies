642e20371c5a182761573ebf865eea9db52a68dd
harrr <alinaami@mail.ru>
2014-03-02 14:03:54 +0400

    #8 Create basic entities

---
+++ models/main/java/Classes/Movie.java
@@ -0,0 +1,45 @@
+public class Movie {
+    private long id;
+    private String title;
+    private String imdbPictureURL;
+    private String imdbId;
+
+    public Movie(long id, String title, String imdbPictureURL, String imdbId) {
+        this.id = id;
+        this.title = title;
+        this.imdbPictureURL = imdbPictureURL;
+        this.imdbId = imdbId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImdbPictureURL() {
+        return imdbPictureURL;
+    }
+
+    public void setImdbPictureURL(String imdbPictureURL) {
+        this.imdbPictureURL = imdbPictureURL;
+    }
+
+    public String getImdbId() {
+        return imdbId;
+    }
+
+    public void setImdbId(String imdbId) {
+        this.imdbId = imdbId;
+    }
+}
---
+++ models/main/java/Classes/User.java
@@ -0,0 +1,46 @@
+import java.util.Date;
+public class User {
+    private long id;
+    private String email;
+    private String password;
+    private Date registrationDate;
+
+    public User(long id, String email, String password, Date registrationDate) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.registrationDate = registrationDate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
